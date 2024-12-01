fc80838ccf10de2e36ac2fdaf01408ce4df14d17
danwritecode <dnelson4993@outlook.com>
2024-12-01 13:07:14 -0500

    day 1 complete

---
+++ src/day1.rs
@@ -0,0 +1,128 @@
+use std::fs::read_to_string;
+use crate::{AdventDay, AdventReturn};
+
+
+pub struct DayOne {
+    file_contents: String
+}
+
+impl AdventDay for DayOne {
+    fn new(input_path: &str) -> Self {
+        let file_contents = read_to_string(input_path).expect("Could not read file contents");
+        DayOne { file_contents } 
+    }
+
+    fn process_pt1(&self) -> AdventReturn {
+        let (set_one, set_two) = &self.parse();
+        let diffs = set_one
+            .iter()
+            .zip(set_two.iter())
+            .map(|(a, b)| a.abs_diff(*b) as i32)
+            .collect::<Vec<i32>>();
+
+        let diff = diffs.iter().sum::<i32>();
+
+        return AdventReturn::Integer(diff);
+    }
+
+    fn process_pt2(&self) -> AdventReturn {
+        let (set_one, set_two) = &self.parse();
+
+        println!("set_one: {:?}", set_one);
+        println!("set_two: {:?}", set_two);
+
+        let score = set_one
+            .iter()
+            .map(|a| {
+                let ct = set_two.iter().filter(|b| a == *b).count();
+                println!("item: {} | ct: {}", a, ct);
+
+                a * ct as i32
+            })
+            .collect::<Vec<i32>>()
+            .iter()
+            .sum::<i32>();
+
+        AdventReturn::Integer(score)
+    }
+}
+
+impl DayOne {
+    fn parse(&self) -> (Vec<i32>, Vec<i32>) {
+        let ( mut set_one, mut set_two ): (Vec<i32>, Vec<i32>) = self.file_contents
+            .lines()
+            .map(|l| {
+                let parts = l.split("   ").collect::<Vec<&str>>();
+                let first = parts[0];
+                let second = parts[1];
+
+                (first.parse::<i32>().unwrap(), second.parse::<i32>().unwrap())
+            })
+            .unzip();
+
+        // sort both
+        set_one.sort();
+        set_two.sort();
+
+        (set_one, set_two)
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn pt_1_sample_input_test() {
+        let day_one = DayOne::new("./inputs/day1/sample");
+        let answer = day_one.process_pt1();
+
+        if let AdventReturn::Integer(i) = answer {
+            assert_eq!(11, i, "Asserting integer response of 11 to match test input");
+            return;
+        }
+
+        panic!("Expected integer type, with value of 11")
+    }
+
+    #[test]
+    fn pt1_full_sample_input_test() {
+        let day_one = DayOne::new("./inputs/day1/full");
+        let answer = day_one.process_pt1();
+
+        if let AdventReturn::Integer(i) = answer {
+            assert_eq!(1506483, i, "Asserting integer response of 1506483 to match test input");
+            return;
+        }
+
+        panic!("Expected integer type, with value of 1506483")
+    }
+
+    #[test]
+    fn pt_2_sample_input_test() {
+        let day_one = DayOne::new("./inputs/day1/sample");
+        let answer = day_one.process_pt2();
+
+        if let AdventReturn::Integer(i) = answer {
+            assert_eq!(31, i, "Asserting integer response of 31 to match test input");
+            return;
+        }
+
+        panic!("Expected integer type")
+    }
+
+    #[test]
+    fn pt2_full_sample_input_test() {
+        let day_one = DayOne::new("./inputs/day1/full");
+        let answer = day_one.process_pt2();
+
+        if let AdventReturn::Integer(i) = answer {
+            assert_eq!(23126924, i);
+            return;
+        }
+
+        panic!("Expected integer type")
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::fmt::Display;
+
+pub mod day1;
+
+#[derive(Debug)]
+enum AdventReturn {
+    String(String),
+    Integer(i32),
+    Float(f64)
+}
+
+impl Display for AdventReturn {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            AdventReturn::String(s) => write!(f, "{}", s),
+            AdventReturn::Integer(i) => write!(f, "{}", i),
+            AdventReturn::Float(fl) => write!(f, "{}", fl),
+        }
+    }
+}
+
+trait AdventDay {
+    fn new(input_path: &str) -> Self;
+    fn process_pt1(&self) -> AdventReturn;
+    fn process_pt2(&self) -> AdventReturn;
+}
+
+fn main() {
+    println!("Hello, advent of code!");
+}
